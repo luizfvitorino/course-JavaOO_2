6b306b4d0d55aa3c200548d2195375fb9d2c8f6d
Luiz Vitorino <luizfvitorinomoreira@gmail.com>
2021-09-12 13:57:47 -0300

    Created attribute "cargo" and changed "bonificacao"

--- src/CriaFuncionario.java
+++ src/CriaFuncionario.java
@@ -2,8 +2,10 @@ public class CriaFuncionario {
     public static void main(String[] args) {
         Funcionario funcionario001 = new Funcionario("Nico Steppat", "434.343.434-44");
         funcionario001.setSalario(2500.00);
+        funcionario001.setCargo(1);
     
         System.out.println(funcionario001.getNome());
+        System.out.println(funcionario001.getCargo());
         System.out.println(funcionario001.getSalario());
         System.out.println(funcionario001.getBonificacao());
     }
