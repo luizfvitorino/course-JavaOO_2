ea3924782380e8c0a327cdd5f810f361a3dd6294
Luiz Vitorino <luizfvitorinomoreira@gmail.com>
2021-09-13 17:44:12 -0300

    Added attribute "login" and changed method "autenticar" in class Gerente

--- src/CriaFuncionario.java
+++ src/CriaFuncionario.java
@@ -1,22 +1,10 @@
 public class CriaFuncionario {
     public static void main(String[] args) {
-        Funcionario funcionario001 = new Funcionario("Nico Steppat", "434.343.434-44");
-        funcionario001.setSalario(2500.00);
-        
         Gerente gerente001 = new Gerente("Carla Andrade", "333.222.333-22");
         gerente001.setSalario(4500.00);
-        gerente001.setSenha("222a");
+        gerente001.setLogin("candrade");
+        gerente001.setSenha("candrade");
         
-        System.out.println(funcionario001.getNome());
-        System.out.println(funcionario001.getSalario());
-        System.out.println(funcionario001.getBonificacao());
-        System.out.println();
-        System.out.println(gerente001.getNome());
-        System.out.println(gerente001.getSalario());
-        System.out.println(gerente001.getBonificacao());
-        System.out.println();
-        
-        boolean gerenteAutenticado = gerente001.autenticar("222a");
-        System.out.println(gerenteAutenticado);
+        gerente001.autenticar("candrade", "candrade");
     }
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,21 +1,45 @@
 import java.util.Objects;
 
 public class Gerente extends Funcionario{
+    private String cargo = "Gerente";
+    private String login;
     private String senha;
     
     public Gerente(String nome, String cpf) {
         super(nome, cpf);
     }
     
+    public String getCargo() {
+        return cargo;
+    }
+    
     public void setSenha(String senha) {
-        this.senha = senha;
+        if (senha.length() > 6 && senha.length() <= 8) {
+            this.senha = senha;
+            System.out.println("Senha setada com sucesso.");
+        } else System.out.println("A sua senha precisa ter entre 6 e 8 caracteres.");
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+    
+    public void setLogin(String login) {
+        if (login.length() > 4 && login.length() <= 12) {
+            this.login = login;
+            System.out.println("Informação de login setada com sucesso.");
+        } else System.out.println("O seu login precisa ter entre 4 e 12 caracteres.");
     }
     
     public double getBonificacao() {
         return super.getBonificacao() + super.getSalario();
     }
     
-    public boolean autenticar(String senha) {
-        return Objects.equals(senha, this.senha);
+    public void autenticar(String login, String senha) {
+        String caller = this.getCargo() + " @" + this.getLogin();
+        
+        if (Objects.equals(login, this.login) && Objects.equals(senha, this.senha)) {
+            System.out.println("Autorizado: Seja bem-vinde, " + caller + "!");
+        } else System.out.println("Não autorizado: Reveja as suas informações de login.");
     }
 }
